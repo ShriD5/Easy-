3dbcfacb7f7c7075c9f335d4283ab578301ca788
ShriD5 <51796064+ShriD5@users.noreply.github.com>
2020-10-21 22:56:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser =require("body-parser");
+const request =require("request");
+
+const app =express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+ "/signup.html");
+});  
+
+
+
+app.post("/",function(req,res){
+var firstName = req.bodyParser.fName; 
+var lastName =req.bodyParser.lName;
+var email =req.bodyParser.email;
+console.log(fName,lName,email);
+});
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
